1de869c862817a5672b751ca5b6879349a2ff6ef
root <root@localhost.localdomain>
2014-05-14 04:03:30 -0700

    13

--- backup.sh
+++ backup.sh
@@ -2,9 +2,9 @@
 #minion_name=`more /etc/salt/minion | grep -v "^#" | grep -v "^$" | grep id | awk '{print $2}'`
 
 mkdir -p /usr/local/backup
-cd /usr/local/backup
+cd /usr/local/backup 
+rm -rf ./backup
 git init
 git clone git@github.com:lipengyu1573/backup.git
 cd ./backup
 sh backup_analyze.sh
-
