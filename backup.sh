962d1d32615300da156b413aedfee02e885d2d6a
lipengyu1573 <lipengyu1573@126.com>
2014-05-15 17:46:43 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 
 salt "*" cmd.run 'echo "StrictHostKeyChecking no" >> ~/.ssh/config'
 salt "*" cmd.run 'yum install git'
-salt "*" cmd.run 'gem install backup'
+salt "*" gem.install backup
 salt "*" 'mkdir -p /usr/local/backup;cd /usr/local/backup;rm -rf ./backup;git init;git clone git@github.com:lipengyu1573/backup.git;cd ./backup;sh backup_analyze.sh' 
 
